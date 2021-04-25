f35d9068f3bc8510172e3b5baa97bfe8981bc421
s.k.noe@hinoshiba.com <s.k.noe@hinoshiba.com>
2021-04-25 19:36:29 +0900

    added menu

--- golang/src/go/src/goplay/exec/qtsample2/main.go
+++ golang/src/go/src/goplay/exec/qtsample2/main.go
@@ -2,16 +2,39 @@ package main
 
 import (
     "github.com/therecipe/qt/widgets"
+    "github.com/therecipe/qt/gui"
     "os"
 )
 
 func main() {
 
     app := widgets.NewQApplication(len(os.Args), os.Args)
+    menu := new(widgets.QMenuBar)
+
+	addAction := widgets.NewQAction2("&Add album...", menu)
+	deleteAction := widgets.NewQAction2("&Delete album...", menu)
+	quitAction := widgets.NewQAction2("&Quit", menu)
+	//aboutAction := widgets.NewQAction2("&About", menu)
+	//aboutQtAction := widgets.NewQAction2("About &Qt", menu)
+
+	addAction.SetShortcut(gui.QKeySequence_FromString("Ctrl+A", 0))
+	deleteAction.SetShortcut(gui.QKeySequence_FromString("Ctrl+D", 0))
+	quitAction.SetShortcuts2(gui.QKeySequence__Quit)
+
+/*
+	fileMenu := menu.AddMenu2("&File")
+	fileMenu.AddActions([]*widgets.QAction{addAction, deleteAction})
+	fileMenu.AddSeparator()
+	fileMenu.AddActions([]*widgets.QAction{quitAction})
+
+	helpMenu := menu.AddMenu2("&Help")
+	helpMenu.AddActions([]*widgets.QAction{aboutAction, aboutQtAction})
+	*/
 
     window := widgets.NewQMainWindow(nil, 0)
     window.SetWindowTitle("Hello World Example")
     window.SetMinimumSize2(200, 200)
+	window.SetMenuBar(menu)
     layout := widgets.NewQVBoxLayout()
 
     mainWidget := widgets.NewQWidget(nil, 0)
